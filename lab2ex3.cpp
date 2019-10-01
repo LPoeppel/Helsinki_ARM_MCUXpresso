3eb2541ea39aa3a4881aca6ac6a142cd21578f96
LPoeppel <43016007+LPoeppel@users.noreply.github.com>
2019-10-02 02:45:18 +0300

    Update lab2ex3.cpp

--- lab2ex3.cpp
+++ lab2ex3.cpp
@@ -2,7 +2,7 @@
 ===============================================================================
  Name        : main.c
  Author      : $lpoeppel
- Version     :
+ Version     : lab2ex3
  Copyright   : $(copyright)
  Description : main definition
 ===============================================================================
@@ -15,11 +15,9 @@
 #include "board.h"
 #endif
 #endif
-#include <string>
-#include <sstream>
-#include <ostream>
-#include <cstring>
-#include "DigitalIoPin.h"
+
+
+
 #include <cr_section_macros.h>
 
 // TODO: insert other include files here
@@ -29,6 +27,7 @@
 
 // TODO: insert other definitions and declarations here
 
+#include <string>
 
 /*****************************************************************************
  * Private types/enumerations/variables
@@ -47,73 +46,95 @@ static void prvSetupHardware(void)
 {
 	SystemCoreClockUpdate();
 	Board_Init();
-
-	/* Initial LED0 state is off */
+	/* Initial greenLED0, redLED1, blueLED2 state is off */
 	Board_LED_Set(0, false);
 	Board_LED_Set(1, false);
+	Board_LED_Set(2, false);
+
 }
 
 SemaphoreHandle_t binary = NULL;
-SemaphoreHandle_t counting = NULL;
-TaskHandle_t xOracleTask = NULL;
-static void vReadTask(void *pvParameters) {
-	std::stringstream output;
-	output << "[YOU] ";
-	std::string input;
-	char c;
-	while(1){
-		int inputc = Board_UARTGetChar();
-		if(inputc != EOF){
-			if(xSemaphoreTake(binary, portMAX_DELAY) == pdTRUE){
-				if(inputc == 13 || inputc == 10 || input.size() == 60){
-							output << input << "\n";
-							Board_UARTPutSTR(output.str().c_str());
-							xSemaphoreGive(binary);
-						}
-				else if(inputc != 13 && inputc != 10){
-					if(inputc == 63){ xTaskNotify(xOracleTask, 1, eIncrement); }
-					c = inputc;
-					input.append(&c);
-				}
-			}else{
-				xSemaphoreGive(binary);
-			}
-		}
-		else{ vTaskDelay(50); }
+SemaphoreHandle_t mutex = NULL;
+SemaphoreHandle_t mutexWrite = NULL;
+
+void chooseOutput(){
+	int i = std::rand() % 5;
+	if (i < 0) {i = -i;}
+	i+= 1;
+
+	switch(i){
+		case 1: Board_UARTPutSTR("[Oracle] A very good question indeed. \r\n");;
+			break;
+		case 2: Board_UARTPutSTR("[Oracle] I will have to think about that. \r\n");;
+			break;
+		case 3: Board_UARTPutSTR("[Oracle] You have too much free time on your hands. \r\n");;
+			break;
+		case 4: Board_UARTPutSTR("[Oracle] Do you want me to answer that, or shall I just listen while you figure it out yourself. \r\n");;
+			break;
+		case 5: Board_UARTPutSTR("[Oracle] The future will bring you the answer. \r\n");;
+			break;
+		default: Board_UARTPutSTR("[Oracle] This seems interesting. \r\n");;
+			break;
 	}
 }
+
+static void vReadTask(void *pvParameters) {
+	std::string input_s = "[You] ";
+	char c = 'a';
+	int input_i = 0;
+	bool value = false;
+	while(1){
+		vTaskDelay(10);
+		input_i = Board_UARTGetChar();
+		if(input_i != EOF){
+			Board_LED_Set(2, true);
+			if(xSemaphoreTake(mutex, portMAX_DELAY) == pdTRUE){
+				if(input_i == 13 || input_i == 10 || input_s.size() >= 60){
+					Board_LED_Set(1, true);
+					input_s.append("\r\n");
+					Board_UARTPutSTR(input_s.c_str());
+					input_s.clear();
+					input_s.append("[You] ");
+					Board_LED_Set(1, false);
+					if(value){
+						value = false;
+						xSemaphoreGive(binary);
+					}
+				}else{
+					if(input_i == 63){
+						value = true;
+					}
+					c = input_i;
+					input_s.push_back(c);
+				}//else
+				Board_LED_Set(2, false);
+			}//if pdTRUE
+		}else{
+			xSemaphoreGive(mutex);
+		}
+
+	}//while (1)
+}//vReadTask
 
 static void vOracleTask(void *pvParameters){
 	while(1){
 		if(xSemaphoreTake(binary, portMAX_DELAY) == pdTRUE){
-			if( xTaskGetHandle("vOracleIndicate") ){
-				Board_UARTPutSTR("[Oracle] Hmmm…");
+			Board_LED_Set(0, false);
+			Board_LED_Set(1, false);
 
-				xSemaphoreGive(binary);
+			Board_LED_Set(2, true);
+			if(xSemaphoreTake(mutex, portMAX_DELAY) == pdTRUE){
+				Board_UARTPutSTR("[Oracle] Hmmmm....\r\n");
+				xSemaphoreGive(mutex);
 				vTaskDelay(3000);
-				if(xSemaphoreTake(binary, portMAX_DELAY) == pdTRUE){
-					std::srand(3);
-					int i = std::rand();
-					std::string output = "[Oracle] ";
-					switch(i){
-						case 0: output.append("This seems like a difficult question...");
-							break;
-						case 1: output.append("You should think about the problem from a different perspective.");
-							break;
-						case 2: output.append("Yes!");
-							break;
-						case 3: output.append("No.");
-							break;
-						default: output.append("This should be thoroughly thought thru..");
-							break;
-					}
-					Board_UARTPutSTR(output.c_str());
-				}
-				xSemaphoreGive(binary);
+				xSemaphoreTake(mutex, portMAX_DELAY);
+				chooseOutput();
+				xSemaphoreGive(mutex);
 				vTaskDelay(2000);
+				Board_LED_Set(2, false);
 			}
 		}else{
-			xSemaphoreGive(binary);
+			xSemaphoreGive(mutex);
 		}
 	}
 }
@@ -141,14 +162,15 @@ int main(void)
 {
 	prvSetupHardware();
 	binary = xSemaphoreCreateBinary();
-	counting = xSemaphoreCreateCounting(5, 0);
+	mutex = xSemaphoreCreateMutex();
+	mutexWrite = xSemaphoreCreateMutex();
 
-	xTaskCreate(vReadTask, "vTaskIndicatRead",
+	xTaskCreate(vReadTask, "vTaskRead",
 					configMINIMAL_STACK_SIZE + 128, NULL, (tskIDLE_PRIORITY + 1UL),
 					(TaskHandle_t *) NULL);
-	xTaskCreate(vOracleTask, "vOracleIndicate",
+	xTaskCreate(vOracleTask, "vTaskOracle",
 					configMINIMAL_STACK_SIZE + 128, NULL, (tskIDLE_PRIORITY + 1UL),
-					&xOracleTask);
+					(TaskHandle_t *) NULL);
 	/* Start the scheduler */
 	vTaskStartScheduler();
 	/* Should never arrive here */
